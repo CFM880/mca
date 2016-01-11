4f56389548768464efbf3d5d3114bd50f0123515
Cheng Fangming <chengfangming1993@gmail.com>
2016-01-11 16:31:49 +0800

    更新文件浏览器主题为Light

--- app/src/main/java/com/example/cfm/myapplication/MyMarkerView.java
+++ app/src/main/java/com/example/cfm/myapplication/MyMarkerView.java
@@ -39,7 +39,7 @@ public class MyMarkerView extends MarkerView {
             tvContent.setText("" + Utils.formatNumber(ce.getHigh(), 0, true));
         } else {
 
-            tvContent.setText("" + Utils.formatNumber(e.getVal(), 0, true));
+            tvContent.setText(Utils.formatNumber(e.getXIndex(),0, true) + ",  "+ Utils.formatNumber(e.getVal(), 0, true));
         }
     }
 
